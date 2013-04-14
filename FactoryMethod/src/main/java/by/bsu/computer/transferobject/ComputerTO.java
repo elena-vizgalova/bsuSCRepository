9571b1d9de1d75e47981414df1fb30a96b42d56e
rollerhel <rollerhel@gmail.com>
2013-04-14 23:22:41 +0300

    Add transfer object class

---
+++ FactoryMethod/src/main/java/by/bsu/computer/transferobject/ComputerTO.java
@@ -0,0 +1,21 @@
+package by.bsu.computer.transferobject;
+
+import java.util.List;
+
+/**
+ *
+ * @author Elena Vizgalova
+ */
+public class ComputerTO {
+    
+    public int cpuCacheSize;
+    public int cpuKernelNumber;
+    public String cpuFirmName;
+    
+    public String motherboardFormFactor;
+    public List<String> motherboardProcessors;
+    
+    public double romReadingSpeed;
+    public String romTypeName;
+    
+}
